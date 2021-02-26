59c2ee73d7fcbd67158c0a469b154883ffca03a1
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-02-26 12:37:56 +0000

    added GET request for products (test)

---
+++ server/routes/products.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req,res,next) => {
+    res.status(200).json({
+        message: "GET request from /products received"
+    });
+});
+
+export default router;
