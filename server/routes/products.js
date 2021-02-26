f56ae2680f06b3c40dae127aac938215e62ed4c1
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-02-26 13:41:59 +0000

    template for simple GET POST PATCH and DELETE requests

--- server/routes/products.js
+++ server/routes/products.js
@@ -7,4 +7,32 @@ router.get('/', (req,res,next) => {
     });
 });
 
+router.post('/', (req,res,next) => {
+    res.status(201).json({
+        message: "GET request from /products received"
+    });
+});
+
+router.get('/:productId', (req,res,next) => {
+    // store id as a variable from "params"
+    // params: thing that is passed via the URL
+    const id = req.params.productId;
+    if (id === 'special'){
+        res.status(200).json({
+            message: "You discovered the special ID"
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req,res,next) => {
+    res.status(201).json({
+        message: "Updated product!",
+        productId: req.params.productId
+    });
+});
+
 export default router;
