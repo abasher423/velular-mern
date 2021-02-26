c5b6d64940405445e28944752b9bd11b0db24093
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-02-26 19:31:23 +0000

    app.js

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
         res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
+    next();
 });
 
 // routes middleware
