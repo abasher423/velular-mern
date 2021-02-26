929e1dc84fdd5abc6fa0ed4113716ff0b7f89794
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-02-26 12:36:45 +0000

    added middleware for productRoutes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import productRoutes from './routes/products.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use('/products', productRoutes);
 
 app.listen(PORT, () => console.log('Server running on port 3000...'));
 
