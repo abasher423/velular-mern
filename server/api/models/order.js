39fb347f977e2fae3d5d6355709a8d53c15cf468
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-02 08:39:48 +0000

    added GET request for a single order

--- server/api/models/order.js
+++ server/api/models/order.js
@@ -5,10 +5,10 @@ const orderSchema = new Schema({
     _id: { type: mongoose.Schema.ObjectId, required: true },
     product: { type: mongoose.Schema.ObjectId, ref: 'Product', required: true },
     currency: { type: String, required: true },
-    quantity: { type: Number, required: true, default: 1 },
-    amount: { type: Number, required: true} ,
+    quantity: { type: Number, default: 1 },
+    amount: { type: Number, required: true } ,
     method: { type: String,  required: true },
-    date: { type: Date, required: true }
+    date: { type: Date, default: Date.now }
 });
 
 export default mongoose.model('Order', orderSchema);
