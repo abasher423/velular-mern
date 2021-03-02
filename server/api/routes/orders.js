08d90067cfecacb00970bedf848756990d6beadb
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-02 08:06:22 +0000

    updated GET request for orders

--- server/api/routes/orders.js
+++ server/api/routes/orders.js
@@ -4,6 +4,39 @@ import Order from '../models/order.js';
 
 const router = express.Router();
 
+// @desc Fetch all orders
+// @route GET /api/orders
+// @access Private
+router.get('/', async (req, res) => {
+    try {
+        const orders = await Order.find();
+        if (orders.length > 0){
+            const response = {
+                count: orders.length,
+                orders: orders.map(order => {
+                    return {
+                        _id: order._id,
+                        productId: order.product,
+                        currency: order.currency,
+                        quantity: order.quantity,
+                        amount: order.amount,
+                        date: order.date,
+                        payment: {
+                            method: order.method
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
+        } else {
+            res.status(404).json({ message: 'No entry exists for orders'});
+        }
+    } catch (err){
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+});
+
 // @desc Create an order
 // @route POST /api/odrders
 // @access Private
