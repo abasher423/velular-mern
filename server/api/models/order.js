dbd2a2ddc6bf39e4f4e584abd3093ba040ba08b3
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-02 23:27:34 +0000

    deleted shipping info from model

--- server/api/models/order.js
+++ server/api/models/order.js
@@ -3,20 +3,12 @@ const { Schema } = mongoose;
 
 const orderSchema = new Schema({
     _id: { type: mongoose.Schema.ObjectId, required: true },
-    productId: { type: mongoose.Schema.ObjectId, ref: 'Product', required: true },
-    products: { type: [String], required: true },
+    product: { type: mongoose.Schema.ObjectId, ref: 'Product', required: true },
     currency: { type: String, required: true },
     quantity: { type: Number, default: 1 },
     amount: { type: Number, required: true } ,
     method: { type: String,  required: true },
     date: { type: Date, default: Date.now },
-    customer: { type: String, required: true },
-    email: { type: String, required: true },
-    street: { type: String, required: true },
-    city: { type: String, requird: true },
-    region: { type: String },
-    state: { type: String, required: true },
-    country: { type: String, required: true },
     complete: { type: Boolean, default: false }
 });
 
