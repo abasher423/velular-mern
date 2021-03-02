e49fce488d0d337f2e0779d1daa94074f7f32a58
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-02 18:48:18 +0000

    added shipping details

--- server/api/models/order.js
+++ server/api/models/order.js
@@ -3,12 +3,21 @@ const { Schema } = mongoose;
 
 const orderSchema = new Schema({
     _id: { type: mongoose.Schema.ObjectId, required: true },
-    product: { type: mongoose.Schema.ObjectId, ref: 'Product', required: true },
+    productId: { type: mongoose.Schema.ObjectId, ref: 'Product', required: true },
+    products: { type: [String], required: true },
     currency: { type: String, required: true },
     quantity: { type: Number, default: 1 },
     amount: { type: Number, required: true } ,
     method: { type: String,  required: true },
-    date: { type: Date, default: Date.now }
+    date: { type: Date, default: Date.now },
+    customer: { type: String, required: true },
+    email: { type: String, required: true },
+    street: { type: String, required: true },
+    city: { type: String, requird: true },
+    region: { type: String },
+    state: { type: String, required: true },
+    country: { type: String, required: true },
+    complete: { type: Boolean, default: false }
 });
 
 export default mongoose.model('Order', orderSchema);
