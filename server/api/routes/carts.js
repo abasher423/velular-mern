76f56cac74ed6aa06c19b8c6ca8cdd7e7a881b3e
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-04 07:13:32 +0000

    added PATCH and DELETE request handlers

--- server/api/routes/carts.js
+++ server/api/routes/carts.js
@@ -96,6 +96,54 @@ router.post('/', (req, res) => {
         })
 });
 
+router.patch('/:cartId', async (req, res) => {
+    try {
+        const updateOps = {}; // objct so if we want to only update certain values we can (change)
+        for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+        }
 
+        const result = await Cart.updateOne({_id: req.params.productId}, {$set: updateOps});
+        res.status(201).json({
+            message: 'cart Successfully Updated',
+            request: {
+                type: 'GET',
+                description: 'Get updated product',
+                url: `http://localhost:300/api/cart/${result._id}`
+            }
+        });
+    } catch (err){
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+});
+
+router.delete('/:cartId', async (req, res) => {
+    try {
+        const result = await Cart.deleteOne({ _id: req.params.cartId} );
+        if (result.n > 0){
+            res.status(200).json({
+                message: 'Deleted cart successfully',
+                request: {
+                    type: 'POST',
+                    description: 'Create a cart',
+                    url: 'http://localhost:3000/api/cart',
+                    body: {
+                        productId: 'ObjctID',
+                        active: 'Boolean',
+                        modified: 'Date',
+                        quantity: 'Number',
+                        total: 'Number'
+                    }
+                }
+            });
+        } else {
+            res.status(404).json({ message: 'No entry exists or already deleted' });
+        }
+    } catch (err){
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+});
 
 export default router;
