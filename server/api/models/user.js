f6892d49fa01409e15250450663e74f68c24c92f
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-04 15:36:10 +0000

    created model for users

---
+++ server/api/models/user.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    _id: { type: mongoose.Schema.ObjectId, required: true },
+    email: { type: String, required: true, unique: true }, //unique doesn't validate values
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    fullName: { type: String, required: true },
+    street: { type: String, required: true},
+    city: { type: String, required: true},
+    country: { type: String, required: true },
+    state: {type: String, required: true },
+    postcode: { type: String, required: true },
+    role: { type: String, enum: ['customer', 'artist', 'admin', 'manager'], required: true },
+    orders: [{ type: mongoose.Schema.ObjectId, ref: 'Order' }]
+});
+
+export default mongoose.model('User', userSchema);
