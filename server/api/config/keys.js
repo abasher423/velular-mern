fb3986a12b595221576df5076df8cd35f6972a15
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-04 15:38:37 +0000

    added user signup router

--- server/api/config/keys.js
+++
@@ -1,3 +0,0 @@
-export default {
-    mongoURI: 'mongodb+srv://brad:brad@e-commerce-app.fdcay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
---
+++ server/api/routes/users.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import User from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'; // hashes and adds salt
+
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length > 0) { // if an email already exists
+                return res.status(422).json({ message: 'An account with the same email already exists' });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({ error: err });
+                    } else {
+                        const { email, fullName, username, street, city, country, state, postcode, role, orderId } = req.body;
+                        const user = new User({ 
+                            _id: mongoose.Types.ObjectId(),
+                            password: hash, 
+                            orders: orderId,
+                            email, username, fullName, street, city, country, state, postcode, role  
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({ message: 'User created' });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({ error: err });
+                            })
+                    }
+                });
+            }
+        })
+});
+
+
+
+export default router;
