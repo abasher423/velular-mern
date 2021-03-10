f6d277e7f4932912a489af84d7d6b1b3db02de57
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-10 17:43:52 +0000

    added carousel

---
+++ client/src/components/SlideShow.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import Link from 'react-router-dom/Link';
+// code from https://www.npmjs.com/package/react-material-ui-carousel
+const SlideShow = () => {
+    return(
+        <Carousel 
+        fullHeightHover={false}     // We want the nav buttons wrapper to only be as big as the button element is
+        navButtonsProps={{          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+            
+        }} 
+        navButtonsWrapperProps={{   // Move the buttons to the bottom. Unsetting top here to override default style.
+            style: {
+                
+                marginLeft: '150px',
+                marginRight: '150px',
+                marginTop: "75px"
+            }
+        }} >
+            <div style={{
+                textAlign: "center",
+                marginTop: "150px",
+                height: "500px"
+                }}>
+                <Link to={'/products'}>
+                    <img className="center" src="/images/air force 1.jpg" alt="airmax"/>
+                </Link>
+            </div>
+            <div style={{
+                textAlign: "center",
+                marginTop: "150px",
+                height: "500px"
+                }}>
+                <Link to={'/products'}>
+                    <img src="/images/airforce-2.jpeg" alt="airmax2" />
+                </Link>
+            </div>
+            <div style={{
+                textAlign: "center",
+                marginTop: "150px",
+                height: "500px"
+                }}>
+                <Link to={'/products'}>
+                    <img src="/images/airforce-1.jpeg" alt="airforce" />
+                </Link>
+            </div>
+        </Carousel>
+    );
+}
+
+export default SlideShow;
