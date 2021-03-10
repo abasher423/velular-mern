4fa462f48742f1f3eebbdfd3f23b85f6017a6f2e
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-10 17:43:00 +0000

    added social media icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,49 @@
+import { Container, Typography } from '@material-ui/core';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import React from 'react';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        backgroundColor: "grey",
+        height: "200px",
+        marginBottom: "1rem",
+        marginTop: "150px"
+    }
+  }));
+
 
 const Footer = () => {
+    const classes = useStyles();
+
     return (
-        <footer>
-            Footer
-        </footer>
+        <Container className="center">
+            <footer className={classes.footer}>
+            <div class="footer-heading footer-1">
+                <h2><Typography variant="h7" color="textSecondary">What's New</Typography></h2>
+                <p>Air Force 1s</p>
+                <p>Vans</p>
+                <p>Air Max</p>
+                <p>Gucci</p>
+            </div>
+            <div class="footer-heading footer-2">
+                <h2><Typography variant="h7" color="textSecondary">Services</Typography></h2>
+                <p>Size Guide</p>
+                <p>Terms and Conditions</p>
+                <p>Privacy Policy</p>
+                <p>Legal</p>
+            </div>
+            <div class="footer-heading footer-2">
+                <h2><Typography variant="h7" color="textSecondary">Socials</Typography></h2>
+                <p><InstagramIcon /></p>
+                <p><TwitterIcon /></p>
+                <p><FacebookIcon/></p>
+            </div>
+            
+            </footer>
+        </Container>
     );
 };
 
