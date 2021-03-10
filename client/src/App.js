4ede95763623b4c9929b377750409b520edb504f
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-10 17:42:45 +0000

    added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import HomePage from './screens/HomePage';
+import ListProducts from './screens/Listedproducts'
+import ProductScreen from './screens/ProductScreen';
 import { Container } from '@material-ui/core';
 
 const App = () => {
   return (
     <>
-    <Header />
-    <main>
-      <Container>
-        <HomePage />
-      </Container>
-    </main>
-    {/* <Footer /> */}
+      <Router>
+          <Header />
+        <main>
+          <Container>
+            <Route path='/' component={HomePage} exact />
+            <Route path='/products' component={ListProducts} exact />
+            <Route path='/products/:productId' component={ProductScreen} />
+          </Container>
+        </main>
+        {/* <Footer /> */}
+        </Router>
     </>
   );
 }
