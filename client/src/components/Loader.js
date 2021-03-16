0974a37910cb39d0f197d200f4c2a7226c3b8d32
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-16 17:45:13 +0000

    created components to load and display messages

---
+++ client/src/components/Loader.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        '& > * + *': {
+          marginTop: theme.spacing(2)
+        },
+    },
+}));
+
+const Loader = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+          <LinearProgress />
+       </div>
+    )
+};
+
+export default Loader;
