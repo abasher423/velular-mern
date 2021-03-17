92c344c08f3115425aebb0567d7acbe9ef417393
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-17 04:48:31 +0000

    completed cart screen

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -4,9 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import { Button, Typography, Avatar, Divider, Paper, List, ListItem, ListItemAvatar, ListItemText, FormControl, Select, MenuItem, IconButton, Card, CardContent } from '@material-ui/core';
+import { Button, Typography, Avatar, Divider, Paper, List, ListItem, ListItemAvatar, ListItemText, FormControl, Select, MenuItem, IconButton, Card, CardContent, CardActions } from '@material-ui/core';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, deleteFromCart } from '../actions/cartActions';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
@@ -38,6 +38,9 @@ const useStyles = makeStyles((theme) => ({
     box: {
         display: "flex",
         justifyContent: "space-between"
+    },
+    btn: {
+        display: "block"
     }
   }));
 
@@ -53,25 +56,46 @@ const CartScreen = ({ match, location, history}) => {
     const { cartItems } = useSelector((state) => state.cart)
 
     const removeFromCartHandler = (productId) => {
-        console.log("remove");
+        dispatch(deleteFromCart(productId));
     };
+
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping');
+    }
     
     return (
         <Container>
-            <Typography variant="h2" component="h2" className={classes.title}>Shopping Cart</Typography>
-            <Card className={classes.root} variant="outlined">
+            <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+                <Typography variant="h2" component="h2" className={classes.title}>Shopping Cart</Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <Card className={classes.root} variant="outlined" align="center">
             <CardContent justify="space-between">
                 <div className={classes.box}>
                     <Typography gutterBottom>Items</Typography>
-                    <Typography variant="h5"gutterBottom>5</Typography>
+                    <Typography variant="h5"gutterBottom>{cartItems.reduce((acc, element) => acc + element.quantity, 0)}</Typography>
                 </div>
                 <Divider style={{margin: "0.5rem 0"}}/>
                 <div className={classes.box}>
                     <Typography gutterBottom>Subtotal</Typography>
-                    <Typography variant="h5"gutterBottom>£10</Typography>
+                    <Typography variant="h5"gutterBottom>£{cartItems.reduce((acc, element) => acc + element.quantity * element.price, 0).toFixed(2)}</Typography>
                 </div>
             </CardContent>
+            <CardActions>
+                <Button 
+                variant="outlined" 
+                color="primary" 
+                size="small" 
+                className={classes.btn}
+                onClick={checkoutHandler} 
+                disabled={cartItems.length === 0}>
+                    Proceed to checkout
+                </Button>
+            </CardActions>
             </Card>
+            </Grid>
+            </Grid>
             {cartItems.length === 0 
                 ?   <div>
                     <Button variant="contained" color="secondary"  component={Link} to={'/products'}style={{ marginBottom: "2rem"}}>go back</Button>
@@ -123,54 +147,3 @@ const CartScreen = ({ match, location, history}) => {
 }
 
 export default CartScreen;
-
-{/* <Card className={classes.root} variant="outlined">
-                            <CardContent justify="space-between">
-                                <div className={classes.box}>
-                                    <Typography gutterBottom>Price</Typography>
-                                    <Typography variant="h5"gutterBottom>{`£${product.price}`}</Typography>
-                                </div>
-                                <Divider style={{margin: "0.5rem 0"}}/>
-                                
-                                <div className={classes.box}>
-                                    <Typography color="success" gutterBottom>Status</Typography>
-                                    <Typography 
-                                    color="success" 
-                                    gutterBottom 
-                                    style={product.quantityInStock > 0 ? {color: "#81c784"} : {color: "red"}}>
-                                        {product.quantityInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                                    </Typography>
-                                </div>
-                                <Divider style={{margin: "0.5rem 0"}}/>
-
-                                <div className={classes.box}>
-                                    <Typography color="success" gutterBottom>Quantity</Typography>
-                                    <FormControl className={classes.formControl}>
-                                        <Select
-                                        labelId="demo-simple-select-label"
-                                        id="demo-simple-select"
-                                        value={quantity}
-                                        onChange={handleQtyChange}
-                                        >
-                                        {
-                                            [...Array(product.quantityInStock).keys()].map(x => (
-                                                <MenuItem key={x + 1} value={x + 1}>{x + 1}</MenuItem>
-                                            ))
-                                        }
-                                        </Select>
-                                    </FormControl>
-                                </div>
-                                <Divider style={{margin: "0.5rem 0"}}/>
-                            </CardContent>
-                            <CardActions>
-                                <Button 
-                                variant="outlined" 
-                                color="primary" 
-                                size="small" 
-                                className={classes.btn}
-                                onClick={addToCartHandler} 
-                                disabled={product.countInStock === 0}>
-                                    Add to Cart
-                                </Button>
-                            </CardActions>
-                            </Card> */}
