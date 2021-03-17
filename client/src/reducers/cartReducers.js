9d129a0f4122fd8638da4c89ee2a0b848dcce153
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-17 00:46:10 +0000

    created cart reducer

---
+++ client/src/reducers/cartReducers.js
@@ -0,0 +1,26 @@
+import {
+    CART_ADD_ITEM,
+    CART_DELETE_ITEM
+} from '../constants/cartConstants';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    switch (action.type){
+        case CART_ADD_ITEM:
+            const item = action.payload;
+            const existItem = state.cartItems.find(x => x.productId === item.productId);
+
+            if (existItem){
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(x => x.productId === existItem.productId ? item : x)
+                };
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                };
+            }
+            default:
+               return state;
+    }
+};
