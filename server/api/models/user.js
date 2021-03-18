276decf82362f3624ee009f1978790910b489d8c
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-18 15:34:30 +0000

    updated response for user login

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -3,10 +3,17 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
     _id: { type: mongoose.Schema.ObjectId, required: true },
-    email: { type: String, required: true, unique: true }, //unique doesn't validate values
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+    }, 
     password: { type: String, required: true },
     username: { type: String, required: true },
-    fullName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     street: { type: String, required: true},
     city: { type: String, required: true},
     country: { type: String, required: true },
