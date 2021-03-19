5d36e25b6ac4f718a22c907701372deee700667c
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-19 05:26:16 +0000

    updated user model

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -8,19 +8,20 @@ const userSchema = new Schema({
         required: true, 
         unique: true,
         match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-
     }, 
     password: { type: String, required: true },
-    username: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    street: { type: String, required: true},
-    city: { type: String, required: true},
-    country: { type: String, required: true },
-    state: {type: String, required: true },
-    postcode: { type: String, required: true },
     role: { type: String, enum: ['customer', 'artist', 'admin', 'manager'], required: true },
     verified: { type: Boolean, default: false }
 });
 
 export default mongoose.model('User', userSchema);
+
+// shipping: {
+//     street: { type: String, required: true},
+//     city: { type: String, required: true},
+//     country: { type: String, required: true },
+//     state: {type: String, required: true },
+//     postcode: { type: String, required: true },
+// }
