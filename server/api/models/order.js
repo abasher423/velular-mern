f4c7f47ca567be4c4120c621a06ae89b2c14bb97
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-03-22 11:05:44 +0000

    added shipping and updated order model

--- server/api/models/order.js
+++ server/api/models/order.js
@@ -9,22 +9,32 @@ const orderSchema = new Schema({
     method: { type: String,  required: true },
     date: { type: Date, default: Date.now },
     user: { type: mongoose.Schema.ObjectId, ref: 'User', requireD: true },
-    products: [{ type: mongoose.Schema.ObjectId, ref: 'Product', required: true }],
-    isComplete: { type: Boolean, default: false },
-    shipping: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-    paymentMethod: { type: String, required: true},
+    orderItems: [{ 
+        productName: { type: String, required: true},
+        quantity: {type: Number, required: true },
+        productImage: { type: String, required: true },
+        product: { type: mongoose.Schema.ObjectId, ref: 'Product', required: true }
+     }],
+    shipping: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postCode: { type: String, required: true },
+        country: { type: String, required: true },
+    },
     paymentResult: {
         _id: { type: String },
         status: { type: String },
         updateTime: { type: String },
         email: { type: String }
     },
+    paymentMethod: { type: String, required: true},
     taxPrice: { type: Number, required: true, default: 0.00 },
     totalPrice: { type: Number, required: true, default: 0.00 },
     shippingPrice: { type: Number, required: true, default: false},
     isPaid: { type: Boolean, required: true, default: false },
     paidAt: { type: Date },
-    DeliveredAt: { type: Date }
+    DeliveredAt: { type: Date },
+    isComplete: { type: Boolean, default: false }
 });
 
 export default mongoose.model('Order', orderSchema);
