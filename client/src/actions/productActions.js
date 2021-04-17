5a7f5e3909cfd2556d5a57c828438455bd8773ef
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2021-04-17 15:15:52 +0100

    deleted product reducer

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -3,10 +3,7 @@ import productServices from '../services/productServices';
 import { 
     PRODUCT_LIST_REQUEST, 
     PRODUCT_LIST_SUCCESS, 
-    PRODUCT_LIST_FAILURE,
-    PRODUCT_DETAILS_REQUEST,
-    PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAILURE
+    PRODUCT_LIST_FAILURE
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) =>{
@@ -20,25 +17,6 @@ export const listProducts = () => async (dispatch) =>{
     } catch (error){
         dispatch({
             type: PRODUCT_LIST_FAILURE,
-            payload: 
-            error.response && error.response.data.message 
-                ? error.response.data.message 
-                : error.message
-        })
-    }
-}
-
-export const listProductDetails = (productId) => async (dispatch) =>{
-    try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST });
-        const { data } = await productServices.indexOne(productId);
-        dispatch({ 
-            type: PRODUCT_DETAILS_SUCCESS,
-            payload: data
-        });
-    } catch (error){
-        dispatch({
-            type: PRODUCT_DETAILS_FAILURE,
             payload: 
             error.response && error.response.data.message 
                 ? error.response.data.message 
